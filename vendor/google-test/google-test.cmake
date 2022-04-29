201232fcfc94569bf97d28e15e65d204e55d52a6
Herman Semenov <GermanAizek@yandex.ru>
2022-04-29 13:06:10 +0300

    Fixed remote error google-test
    Maintainers changed branch to `main`
    Reference: https://github.com/google/googletest/issues/3663

--- vendor/google-test/google-test.cmake
+++ vendor/google-test/google-test.cmake
@@ -1,5 +1,5 @@
-ExternalProject_Add(googletest
-    GIT_REPOSITORY https://github.com/google/googletest
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
+externalproject_add(googletest
+		GIT_REPOSITORY  https://github.com/google/googletest
+		GIT_TAG			main
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
 )
-
